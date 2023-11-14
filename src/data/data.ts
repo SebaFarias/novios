15c8df4354bc957a19684b32678a0b113bd05027
sebafarias <sebastian.fariasb@usach.cl>
2023-11-14 16:04:40 -0300

    data - added mock items

---
+++ src/data/data.ts
@@ -0,0 +1,24 @@
+import { Regalo } from "@/components/RegaloCard";
+
+export const REGALOS : Regalo[] = [
+  {
+    nombre: "Abrazo",
+    precio: 10000,
+    descripcion: "Un rico abrazo"
+  },
+  {
+    nombre: "Saludo",
+    precio: 5000,
+    descripcion: "Nunca está de más saludarnos"
+  },
+  {
+    nombre: "Cena",
+    precio: 50000,
+    descripcion: "Regálanos un momento especial"
+  },
+  {
+    nombre: "Viaje",
+    precio: 100000,
+    descripcion: "Obsequianos una experiencia inolvidable"
+  },
+] 
