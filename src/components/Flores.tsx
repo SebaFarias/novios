a2afd50ac04639a56cff04a4ee4ad15f94107e56
sebafarias <sebastian.fariasb@usach.cl>
2023-11-19 11:21:40 -0300

    smaller card

--- src/components/Flores.tsx
+++ src/components/Flores.tsx
@@ -7,7 +7,7 @@ export interface FloresProps{}
 
 const Flores: React.FunctionComponent<FloresProps> = props => {
   return(
-    <div className='relative h-[100px] md:h-[200px] w-full z-0 md:translate-y-20'>
+    <div className='relative h-[75px] md:h-[200px] w-full z-0 md:translate-y-20'>
       <div className="absolute inset-0">
         <div className=" relative h-full w-full translate-x-20 -translate-y-10 md:translate-x-40 md:-translate-y-20">
           <Image
@@ -40,7 +40,7 @@ const Flores: React.FunctionComponent<FloresProps> = props => {
         </div>
           </div>
       <div className="absolute inset-0">
-      <div className="relative h-full w-full z-0 translate-x-16 -translate-y-4 md:translate-x-28 md:-translate-y-10">
+      <div className="relative h-full w-full z-0 translate-x-12 -translate-y-2 md:translate-x-28 md:-translate-y-10">
           <Image
             src={Tulipan}
             alt=''
@@ -50,7 +50,7 @@ const Flores: React.FunctionComponent<FloresProps> = props => {
         </div>
           </div>
       <div className="absolute inset-0">
-      <div className=" relative h-full w-full  z-0 -scale-x-100 -translate-x-16 -translate-y-4 md:-translate-x-28 md:-translate-y-10">
+      <div className=" relative h-full w-full  z-0 -scale-x-100 -translate-x-12 -translate-y-2 md:-translate-x-28 md:-translate-y-10">
           <Image
             src={Tulipan}
             alt=''
