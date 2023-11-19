0e27052e5b236a265f6d1f6821c45e20cfd5fe62
sebafarias <sebastian.fariasb@usach.cl>
2023-11-19 09:54:07 -0300

    feature - less saturated color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero : React.FunctionComponent<HeroProps> = props => {
       <NoviosNames/>
     </div>
     <Flores/>
-    <a href="/#regalos" className="text-xl border-4 border-pink-600 text-pink-600 rounded-full px-4 py-2 hover:bg-pink-100 z-20 font-bold">V</a>
+    <a href="/#regalos" className="text-xl border-4 border-pink-600 text-pink-600 rounded-full px-4 py-2 hover:bg-pink-100 z-20 font-bold opacity-50">V</a>
       <h2 className="text-center mt-16 font-serif text-gray-500">Tu compañía es nuestro mejor regalo </h2>
   </section>
   )
