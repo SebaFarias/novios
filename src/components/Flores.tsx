ab2bd133109b3ff6f380c3d3b9506cc5cc328d08
sebafarias <sebastian.fariasb@usach.cl>
2023-11-19 09:54:47 -0300

    feature - flower fixed size

--- src/components/Flores.tsx
+++ src/components/Flores.tsx
@@ -7,8 +7,19 @@ export interface FloresProps{}
 
 const Flores: React.FunctionComponent<FloresProps> = props => {
   return(
-    <div className='grid grid-cols-3 h-[20vh] w-full grid-rows-1 z-0 '>
-      <div className=" relative h-[20vh] w-full col-start-2  row-start-1 translate-x-16 -translate-y-10 md:translate-x-40 md:-translate-y-28">
+    <div className='relative h-[100px] md:h-[200px] w-full z-0 '>
+      <div className="absolute inset-0">
+        <div className=" relative h-full w-full translate-x-20 -translate-y-10 md:translate-x-40 md:-translate-y-20">
+          <Image
+            src={Hoja}
+            alt=''
+            fill
+            style={{ objectFit: 'contain'}}
+            />
+        </div>
+        </div>
+      <div className="absolute inset-0">
+      <div className="relative w-full h-full -scale-x-100 -translate-x-20 -translate-y-10 md:-translate-x-40 md:-translate-y-20">
           <Image
             src={Hoja}
             alt=''
@@ -16,24 +27,20 @@ const Flores: React.FunctionComponent<FloresProps> = props => {
             style={{ objectFit: 'contain'}}
           />
         </div>
-      <div className="relative h-[20vh] w-full col-start-2  row-start-1 -scale-x-100 -translate-x-16 -translate-y-10 md:-translate-x-40 md:-translate-y-28">
+          </div>
+      <div className="absolute inset-0">
+      <div className="relative w-full h-full  z-10 row-start-1 ">
           <Image
-            src={Hoja}
-            alt=''
-            fill
-            style={{ objectFit: 'contain'}}
-          />
-        </div>
-      <div className="relative h-[20vh] w-full col-start-2 z-10 row-start-1 ">
-          <Image
-            className="latiendo"
+            className=""
             src={Flor}
             alt=''
             fill
             style={{ objectFit: 'contain'}}
           />
         </div>
-      <div className="relative h-[20vh] w-full col-start-2 z-0 row-start-1 translate-x-12 -translate-y-4 md:translate-x-32 md:-translate-y-10">
+          </div>
+      <div className="absolute inset-0">
+      <div className="relative h-full w-full z-0 translate-x-16 -translate-y-4 md:translate-x-28 md:-translate-y-10">
           <Image
             src={Tulipan}
             alt=''
@@ -41,14 +48,17 @@ const Flores: React.FunctionComponent<FloresProps> = props => {
             style={{ objectFit: 'contain'}}
           />
         </div>
-      <div className=" relative h-[20vh] w-full col-start-2 z-0 row-start-1 -scale-x-100 -translate-x-12 -translate-y-4 md:-translate-x-32 md:-translate-y-10">
+          </div>
+      <div className="absolute inset-0">
+      <div className=" relative h-full w-full  z-0 -scale-x-100 -translate-x-16 -translate-y-4 md:-translate-x-28 md:-translate-y-10">
           <Image
             src={Tulipan}
             alt=''
             fill
             style={{ objectFit: 'contain'}}
-          />
+            />
         </div>
+      </div>
     </div>
   )
 }
