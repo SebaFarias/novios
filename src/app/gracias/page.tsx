82b6f5ad90af96e62bcf3c4f9cad9666f7e4631c
sebafarias <sebastian.fariasb@usach.cl>
2023-11-19 10:51:09 -0300

    feature - mobile font-size fixed

--- src/app/gracias/page.tsx
+++ src/app/gracias/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around px-2">
       <div className="">
-        <h1 className="text-center text-2xl md:text-7xl font-serif text-amber-950 opacity-70">Gracias por tu aporte</h1>
+        <h1 className="text-center text-4xl md:text-7xl font-serif text-amber-950 opacity-70 font-medium">Gracias por tu aporte</h1>
         <h1 className="text-center text-sm md:text-2xl mt-2 md:mt-8 font-serif text-amber-950 opacity-70">SÓLO QUEDA UN PASO, DEPOSITA EL MONTO EN ESTA CUENTA:</h1>
       </div>
       <Cuenta/>
