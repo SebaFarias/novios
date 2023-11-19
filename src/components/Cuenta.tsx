019cc7753824bd6049e15ddd33aa5ab2dcafad4a
sebafarias <sebastian.fariasb@usach.cl>
2023-11-19 16:14:50 -0300

    feature - new lighter less saturated style

--- src/components/Cuenta.tsx
+++ src/components/Cuenta.tsx
@@ -1,11 +1,11 @@
 import { FunctionComponent } from "react"
 
 const Cuenta: FunctionComponent = props => {
-  return(<section className='text-xl text-center mx-auto bg-[#8C6238] text-[#F0E5DB] w-[230px] sm:w-[340px] list-none border-4 border-[#D5B699] hover:scale-105 transition-all duration-300 font-serif px-5 md:px-6 py-4 '>
-    <p>Banco Itaú</p>
-    <p>Cuenta cte 0220990769</p>
-    <p>Andrea Díaz Ubilla</p>
-    <p>18.169.168-9</p>
+  return(<section className='drop-shadow-md hover:drop-shadow-2xl text-xl text-center text-amber-950 mx-auto papel rounded-2xl w-[230px] sm:w-[340px] list-none border-4 border-[#D5B699] hover:scale-105 transition-all duration-300 font-serif px-5 md:px-6 py-4 '>
+    <p className="opacity-60">Banco Itaú</p>
+    <p className="opacity-60">Cuenta cte 0220990769</p>
+    <p className="opacity-60">Andrea Díaz Ubilla</p>
+    <p className="opacity-60">18.169.168-9</p>
   </section>)
 }
 
