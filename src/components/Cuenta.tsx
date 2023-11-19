747ba14350fc89efb355f7628a158e6892ec0883
sebafarias <sebastian.fariasb@usach.cl>
2023-11-19 11:21:30 -0300

    feature - bigger card

--- src/components/Cuenta.tsx
+++ src/components/Cuenta.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from "react"
 
 const Cuenta: FunctionComponent = props => {
-  return(<section className='text-xl text-center mx-auto bg-[#8C6238] text-[#F0E5DB] w-[230px] sm:w-[340px] list-none p-5 border-4 border-[#D5B699] hover:scale-105 transition-all duration-300 font-serif px-5 py-4 '>
+  return(<section className='text-xl text-center mx-auto bg-[#8C6238] text-[#F0E5DB] w-[230px] sm:w-[340px] list-none border-4 border-[#D5B699] hover:scale-105 transition-all duration-300 font-serif px-6 py-4 '>
     <p>Banco Itaú</p>
     <p>Cuenta cte 0220990769</p>
     <p>Andrea Díaz Ubilla</p>
