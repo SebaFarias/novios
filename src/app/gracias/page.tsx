915beab9922e9eff769a0843d7822e56810af431
sebafarias <sebastian.fariasb@usach.cl>
2023-11-19 09:54:26 -0300

    padding

--- src/app/gracias/page.tsx
+++ src/app/gracias/page.tsx
@@ -2,7 +2,7 @@ import Cuenta from "@/components/Cuenta";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-around">
+    <main className="flex min-h-screen flex-col items-center justify-around px-2">
       <div className="">
         <h1 className="text-center text-2xl md:text-7xl font-serif text-slate-600">Gracias por tu aporte</h1>
         <h1 className="text-center text-sm md:text-2xl mt-2 md:mt-8 font-serif text-slate-600">SÓLO QUEDA UN PASO, DEPOSITA EL MONTO EN ESTA CUENTA:</h1>
