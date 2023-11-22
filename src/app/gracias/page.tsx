8d073a1cafaca93d18157a971e73537f1c3d6707
sebafarias <sebastian.fariasb@usach.cl>
2023-11-22 14:51:09 -0300

    layout shift

--- src/app/gracias/page.tsx
+++ src/app/gracias/page.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around px-2">
-      <div className="">
-        <h1 className="text-center text-4xl md:text-7xl font-serif text-amber-950 opacity-70 font-medium">Gracias por tu aporte</h1>
-        <h1 className="text-center text-sm md:text-2xl mt-2 md:mt-8 font-serif text-amber-950 opacity-70">SÓLO QUEDA UN PASO, DEPOSITA EL MONTO EN ESTA CUENTA:</h1>
-      </div>
+      <h1 className="text-center text-4xl md:text-7xl font-serif text-amber-950 opacity-70 font-medium">Gracias por tu aporte</h1>
       <SelectedCardShower/>
+      <h1 className="text-center text-sm md:text-2xl mt-2 md:mt-8 font-serif text-amber-950 opacity-70">SÓLO QUEDA UN PASO, DEPOSITA EL MONTO EN ESTA CUENTA:</h1>
       <Cuenta/>
-      <Link href="/" className="font-serif border-2 border-[#D5B699]  py-2 px-6 rounded text-[#9D8373] hover:text-[#8A6C5C] bg-transparent hover:bg-[#F0D7C1]  transition-all " >VOLVER</Link>
+      <Link href="/" className="font-serif border-2 border-[#D5B699]  py-2 px-6 rounded text-[#9D8373] hover:text-[#8A6C5C] bg-transparent hover:bg-[#F0D7C1]  transition-all " >
+        VOLVER
+      </Link>
     </main>
   )
 }
